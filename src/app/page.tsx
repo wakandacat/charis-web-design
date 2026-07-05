35e8c490682e612f4f58a425e9938814adff20d5
Ellena Tzavelas <wakandacat@gmail.com>
2026-07-05 16:28:00 -0400

    setup file structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-
+//the Homepage
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
---
+++ src/app/services/page.tsx
@@ -0,0 +1,1 @@
+//the Services page
