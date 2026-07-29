1c426cfb9cdede9d0c396da9c36e94ef0a874204
Ellena Tzavelas <wakandacat@gmail.com>
2026-07-29 15:14:34 -0400

    Footer branch (#1)
    * navbar modifications and styling
    
    * footer functional, styled, responsive, added new global styles, touched navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-(--charis-white)">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16  bg-(--charis-yellow)">
+      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16  bg-(--charis-light)">
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-(--color-text)">
             hey guys its me
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,6 +4,8 @@ import Link from "next/link";
 import Image from "next/image";
 import { useIsSmallScreen } from "@/components/screen-size-provider";
 import { useEffect, useState } from "react";
+import { X, Menu } from "lucide-react";
+import { usePathname } from "next/navigation";
 
 //navbar component to be used on all pages
 //display the site icon
@@ -14,6 +16,9 @@ import { useEffect, useState } from "react";
 export default function Navbar() {
   //check the screen size
   const isSmallScreen = useIsSmallScreen();
+
+  //get the currrent page path
+  const pathname = usePathname();
 
   useEffect(() => {
     // close the menu if the screen size changes to large
@@ -27,78 +32,150 @@ export default function Navbar() {
 
   if (isSmallScreen) {
     return (
-      <nav className="bg-background flex flex-col items-center font-sans justify-between mx-4">
-        <span className="flex flex-row">
-          <span className="flex flex-row p-4 justify-center items-center gap-2 text-lg">
-            <Image
-              src="/icon.png"
-              alt="Charis Web Design Icon"
-              className="m-4"
-              width={50}
-              height={50}
-              priority
-            />
-            <p className="font-serif text-3xl">Charis Web Design</p>
-          </span>
-          <a onClick={() => setMenuOpen(!menuOpen)}>
-            <Image
-              src="/"
-              alt="Toggle Menu"
-              className="m-4"
-              width={50}
-              height={50}
-              priority
-            />
-          </a>
-        </span>
-        {menuOpen && (
-          <span className="bg-background flex flex-col items-center font-sans justify-between mx-4 p-4">
-            <Link className="hover:text-(--charis-yellow)" href="/">
-              Home
+      <header>
+        <nav className="bg-background px-6 py-4 mx-auto w-full max-w-7xl relative">
+          <div className="flex flex-row items-center justify-between w-full">
+            <Link
+              className="flex flew-row items-center"
+              href="/"
+              onClick={() => setMenuOpen(false)}
+            >
+              <Image
+                src="/icon.png"
+                alt="Charis Web Design Icon"
+                className="pr-2"
+                width={50}
+                height={50}
+                priority
+              />
+              <p className="font-serif text-2xl">Charis Web Design</p>
             </Link>
-            <Link className="hover:text-(--charis-yellow)" href="/projects">
-              Projects
-            </Link>
-            <Link className="hover:text-(--charis-yellow)" href="/services">
-              Services
-            </Link>
-            <Link className="hover:text-(--charis-yellow)" href="/contact">
-              Contact
-            </Link>
-          </span>
-        )}
-      </nav>
+            <a onClick={() => setMenuOpen(!menuOpen)}>
+              {menuOpen ? (
+                <X size={30} strokeWidth={1.5} />
+              ) : (
+                <Menu size={30} strokeWidth={1.5} />
+              )}
+            </a>
+          </div>
+
+          {menuOpen && (
+            <div className="absolute left-0 top-full w-full z-10">
+              <ul className="bg-background flex flex-col items-center font-sans justify-between gap-4 border border-(--charis-yellow) p-4 text-lg">
+                <Link
+                  className={`hover:text-(--charis-yellow) ${
+                    pathname === "/"
+                      ? "text-(--charis-yellow)"
+                      : "text-(--charis-white)"
+                  }`}
+                  href="/"
+                  onClick={() => setMenuOpen(false)}
+                >
+                  Home
+                </Link>
+                <Link
+                  className={`hover:text-(--charis-yellow) ${
+                    pathname === "/projects"
+                      ? "text-(--charis-yellow)"
+                      : "text-(--charis-white)"
+                  }`}
+                  href="/projects"
+                  onClick={() => setMenuOpen(false)}
+                >
+                  Projects
+                </Link>
+                <Link
+                  className={`hover:text-(--charis-yellow) ${
+                    pathname === "/services"
+                      ? "text-(--charis-yellow)"
+                      : "text-(--charis-white)"
+                  }`}
+                  href="/services"
+                  onClick={() => setMenuOpen(false)}
+                >
+                  Services
+                </Link>
+                <Link
+                  className={`hover:text-(--charis-yellow) ${
+                    pathname === "/contact"
+                      ? "text-(--charis-yellow)"
+                      : "text-(--charis-white)"
+                  }`}
+                  href="/contact"
+                  onClick={() => setMenuOpen(false)}
+                >
+                  Contact
+                </Link>
+              </ul>
+            </div>
+          )}
+        </nav>
+      </header>
     );
   } else {
     return (
-      <nav className="bg-background flex flex-row items-center font-sans justify-between mx-20">
-        <span className="flex flex-row p-4 justify-center items-center gap-2 text-lg">
-          <Image
-            src="/icon.png"
-            alt="Charis Web Design Icon"
-            className="m-4"
-            width={50}
-            height={50}
-            priority
-          />
-          <p className="font-serif text-3xl">Charis Web Design</p>
-        </span>
+      <header>
+        <nav className="bg-background px-6 py-4 mx-auto w-full max-w-7xl">
+          <span className="flex flex-row items-center w-full">
+            <Link href="/">
+              <div className="flex flex-row items-center">
+                <Image
+                  src="/icon.png"
+                  alt="Charis Web Design Icon"
+                  className="pr-2"
+                  width={50}
+                  height={50}
+                  priority
+                />
+                <p className="font-serif text-3xl">Charis Web Design</p>
+              </div>
+            </Link>
 
-        <span className="flex flex-row p-4 justify-center items-center gap-8 text-lg">
-          <Link className="hover:text-(--charis-yellow)" href="/">
-            Home
-          </Link>
-          <Link className="hover:text-(--charis-yellow)" href="/projects">
-            Projects
-          </Link>
-          <Link className="hover:text-(--charis-yellow)" href="/services">
-            Services
-          </Link>
-          <Link className="hover:text-(--charis-yellow)" href="/contact">
-            Contact
-          </Link>
-        </span>
-      </nav>
+            <ul className="flex flex-row gap-4 text-lg font-sans justify-center items-baseline ml-auto">
+              <Link
+                className={`hover:text-(--charis-yellow) ${
+                  pathname === "/"
+                    ? "text-(--charis-yellow)"
+                    : "text-(--charis-white)"
+                }`}
+                href="/"
+              >
+                Home
+              </Link>
+              <Link
+                className={`hover:text-(--charis-yellow) ${
+                  pathname === "/projects"
+                    ? "text-(--charis-yellow)"
+                    : "text-(--charis-white)"
+                }`}
+                href="/projects"
+              >
+                Projects
+              </Link>
+              <Link
+                className={`hover:text-(--charis-yellow) ${
+                  pathname === "/services"
+                    ? "text-(--charis-yellow)"
+                    : "text-(--charis-white)"
+                }`}
+                href="/services"
+              >
+                Services
+              </Link>
+              <Link
+                className={`hover:text-(--charis-yellow) ${
+                  pathname === "/contact"
+                    ? "text-(--charis-yellow)"
+                    : "text-(--charis-white)"
+                }`}
+                href="/contact"
+              >
+                Contact
+              </Link>
+            </ul>
+          </span>
+        </nav>
+      </header>
     );
   }
 }
